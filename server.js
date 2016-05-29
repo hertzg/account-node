3c08d90cb5c2cf596ebc6338da0621202d8d2ee4
Archil Imnadze <archil@imnadze.ge>
2016-05-29 10:51:03 +0000

    dump and restore user data

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var http = require('http'),
+function shutdown () {
+    for (var i in users) {
+        fs.writeFileSync('dump/' + i, JSON.stringify(users[i]))
+    }
+    process.exit()
+}
+
+var fs = require('fs'),
+    http = require('http'),
     url = require('url')
 
 var config = require('./config.js'),
     Error404Page = require('./lib/Error404Page.js')
 
 var users = Object.create(null)
+fs.readdirSync('dump').forEach(username => {
+    if (username === '.gitignore') return
+    var content = fs.readFileSync('dump/' + username, 'utf8')
+    users[username] = JSON.parse(content)
+})
 
 var pages = Object.create(null)
 pages['/'] = require('./lib/IndexPage.js')
@@ -17,3 +30,6 @@ http.createServer((req, res) => {
     if (page === undefined) page = Error404Page
     page(req, res, parsedUrl)
 }).listen(config.port, config.host)
+
+process.on('SIGINT', shutdown)
+process.on('SIGTERM', shutdown)
